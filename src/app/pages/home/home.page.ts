8c9d0d5982adb6ed76d2810e47df8aadf5bef447
Tim Biesenbeek <MakerTim@outlook.com>
2019-08-07 10:14:32 +0200

    Made homepage USEFULL, added for servicedeskmanagers buttons to go directly to their page

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,18 +1,36 @@
 // tslint:disable:component-selector
 // tslint:disable:component-class-suffix
-import {Component, ElementRef, ViewChild} from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {GLOBAL} from '../../services/global';
+import {getUser} from '../../services/login.service';
 
 @Component({
 	selector: 'home',
 	templateUrl: './home.page.html',
 	styleUrls: ['./home.page.scss']
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
 	@ViewChild('user') userElement: ElementRef;
 	@ViewChild('pass') passElement: ElementRef;
 
 	public glpiURL = GLOBAL.glpiUrl;
+	public isAdmin = false;
+	public groups = [];
 
+	ngOnInit() {
+		getUser(user => {
+			// tslint:disable-next-line:no-bitwise
+			if (((user.rights.ticket) & 1) === 1) {
+				this.isAdmin = true;
+				if (user.groups) {
+					this.groups = user.groups;
+				}
+			}
+		});
+	}
+
+	hasMultipleGroups() {
+		return this.groups.length > 1;
+	}
 }
